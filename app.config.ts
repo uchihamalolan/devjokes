c9be8112db5ae6b7cd8c24fe999df407891777e1
Malolan Balaji <mbalaji@atlassian.com>
2025-05-17 23:24:05 +0530

    add netlify preset

--- app.config.ts
+++ app.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "@tanstack/react-start/config";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+	server: {
+		preset: "netlify",
+	},
 	vite: {
 		plugins: [
 			tailwindcss(),
