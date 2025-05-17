98a9f0e4223a64e148af2800a624bfc9b2343dbe
Malolan Balaji <mbalaji@atlassian.com>
2025-05-17 21:01:16 +0530

    adds tailwind

--- app.config.ts
+++ app.config.ts
@@ -1,9 +1,11 @@
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "@tanstack/react-start/config";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
 	vite: {
 		plugins: [
+			tailwindcss(),
 			tsConfigPaths({
 				projects: ["./tsconfig.json"],
 			}),
